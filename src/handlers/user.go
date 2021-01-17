3557cf95f02de291e81b4032739e4ed601e67e43
Jack Fox <0xdeadbeef1@gmail.com>
2021-01-17 00:44:37 -0500

    Made the user route use the auth middleware

--- src/handlers/user.go
+++ src/handlers/user.go
@@ -1,8 +1,15 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"limecord-core/middleware"
+)
+
+func getUser(ctx *gin.Context) {
+
+}
 
 // register the routers for the tracking module
 func RegisterUser(router *gin.RouterGroup) {
-	router.GET("/users/:user", )
+	router.GET("/users/:user", middleware.AuthMiddleware, getUser)
 }
